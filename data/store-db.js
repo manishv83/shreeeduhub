bffde002f919214e6ab1c9b7265ea31a59bd6b5c
manishv83 <50191094+manishv83@users.noreply.github.com>
2026-04-05 18:39:42 +0530

    Add files via upload

---
+++ data/store-db.js
@@ -0,0 +1,55 @@
+const storefrontDatabase = [
+     {
+        id: "p1",
+        category: ["all","primary"],
+        subjects: ["English", "Grammar"], 
+        title: "Wren & Martin Grammar",
+        desc: "Improve your English writing and grammar skills for all classes.",
+        image: "https://m.media-amazon.com/images/I/716i1tRy01L._SL1500_.jpg",
+        link: "https://amzn.to/4dOcZl9",
+        badge: "Essential"
+    },
+
+    {
+        id: "s1",
+        category: ["all","secondary"],
+        subjects: ["English", "Grammar"],  
+        title: "Wren & Martin Grammar",
+        desc: "Improve your English writing and grammar skills for all classes.",
+        image: "https://m.media-amazon.com/images/I/616tJ8oyRRL._SL1500_.jpg",
+        link: "https://amzn.to/4tvDY9v",
+        badge: "Expert Choice"
+    },
+    {
+        id: "h1",
+        category: ["all","higher"],
+        subjects: ["English"], 
+        title: "Wren & Martin Grammar",
+        desc: "Best Grammar Practice Book for Students - S Chand School Education",
+        image: "https://m.media-amazon.com/images/I/71n4b8nRbFL._SL1500_.jpg",
+        link: "https://amzn.to/4mbZ4Y5",
+        badge: "Best Seller"
+    },
+    {
+        id: "h2",
+        category: ["all","secondary"],
+        subjects: ["Math"], 
+        title: "R.D. Sharma Class 10 Math",
+        desc: "The gold standard for CBSE and Bihar Board Mathematics prep.",
+        image: "https://m.media-amazon.com/images/I/818myWdI5pL._SL1500_.jpg",
+        link: "https://amzn.to/4sbs5EB",
+        badge: "Top Rated"
+    },
+    {
+    id: "ugp1",
+    category: "graduation",
+    subjects: "Physics",
+    title: "Physics of Waves and Oscillation",
+    desc: "Wonderful book on waves and oscillations.",
+    image: "https://m.media-amazon.com/images/I/71IOSlA9j8L._SL1500_.jpg",
+    link: "https://amzn.to/4tzTMIx",
+    badge: "Expert Choice"
+},
+   
+     
+];
