c491d066eb38be49ec675c21009b18b14e16fa80
manishv83 <50191094+manishv83@users.noreply.github.com>
2026-04-14 19:12:31 +0530

    Add files via upload

--- data/store-db.js
+++ data/store-db.js
@@ -1,7 +1,7 @@
 const storefrontDatabase = [
     {
         id: "m1",
-        category: ["all","moral", "primary"],
+        category: ["all","moral","primary"],
         subjects: ["English"], 
         title: "101 Tales The Great Panchatantra Collection",
         desc: "This compilation of 101 tales from the Panchatantra aims to guide the young readers about wise conduct, human nature, and the right path for attaining success in life.",
@@ -9,7 +9,80 @@ const storefrontDatabase = [
         link: "https://amzn.to/4vLxFkj",
         badge: "Best Seller"
     },
-    {
+     {
+        id: "m2",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "The Wildest Thing by Emily Winfield Martin",
+        desc: "This book is the ideal addition to any bedtime reading routine or read aloud. The Wildest Thing beautifully expresses a timeless message about little ones unleashing their inner wild and encouraging their budding imagination and unique individuality.",
+        image: "https://m.media-amazon.com/images/I/81U5PbKuN2L._SL1500_.jpg",
+        link: "https://amzn.to/4tJYH9J",
+        badge: "Trending 2026 Releases"
+    },
+{
+        id: "m3",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "Better than a Touchdown by Jalen Hurts",
+        desc: "From Philadelphia Eagles quarterback Jalen Hurts comes an empowering story about friendship, the power of teamwork, and achieving goals together.",
+        image: "https://m.media-amazon.com/images/I/81Ah5CRMB1L._SL1500_.jpg",
+        link: "https://amzn.to/4cqjwA7",
+        badge: "Trending"
+    },
+{
+        id: "m4",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "Maanas : Jeevan Vyavhaar",
+        desc: "Understanding Paap, Punya, and the Philosophy of Human Conduct| Hindi|.",
+        image: "https://m.media-amazon.com/images/I/51P7ak-buvL._SL1080_.jpg",
+        link: "https://amzn.to/4cGGlRg",
+        badge: "Religious book "
+    },
+
+{
+        id: "m5",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "108 Panchatantra Stories (Illustrated) ",
+        desc: "Readers of all age groups enjoy reading and re-reading the Panchatantra Stories as they impart profound wisdom and moral values about the conduct of life through simple narratives. ",
+        image: "https://m.media-amazon.com/images/I/81VJ+MAc7YL._SL1500_.jpg",
+        link: "https://amzn.to/4cEeEbL",
+        badge: "Bedtime Stories"
+    },
+{
+        id: "m6",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "Wren & Martin Grammar",
+        desc: "This comedic masterpiece is a whimsical exploration of societal conventions, mistaken identities, and the absurdities of Victorian society, guaranteed to leave you laughing out loud.",
+        image: "https://m.media-amazon.com/images/I/71czCCueUaL._SL1500_.jpg",
+        link: "https://amzn.to/48AiX5t",
+        badge: "Essential"
+    },
+
+{
+        id: "m7",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "108 Indian Mythology Stories",
+        desc: "108 stories is a unique book which contains some very interesting and fascinating stories from the domain of Hindu Gods and Goddesses. ",
+        image: "https://m.media-amazon.com/images/I/81iTTgn0lDL._SL1500_.jpg",
+        link: "https://amzn.to/4t5WO7f",
+        badge: "Religious"
+    },
+{
+        id: "m8",
+        category: ["all","moral","primary"],
+        subjects: ["English"], 
+        title: "The Voice of Knowledge: A Practical Guide to Inner Peace",
+        desc: "In The Voice of Knowledge, Miguel Ruiz reminds us of a profound and simple truth.",
+        image: "https://m.media-amazon.com/images/I/9171j5UL3OL._SL1500_.jpg",
+        link: "https://amzn.to/4tPA5wo",
+        badge: "Essential"
+    },
+
+   {
         id: "p1",
         category: ["all","primary"],
         subjects: ["English", "Grammar"], 
@@ -103,8 +176,8 @@ const storefrontDatabase = [
     },
     {
     id: "ugp1",
-    category: "graduation",
-    subjects: "Physics",
+    category:["all", "graduation"],
+    subjects: ["Physics"],
     title: "Physics of Waves and Oscillation",
     desc: "Wonderful book on waves and oscillations.",
     image: "https://m.media-amazon.com/images/I/71IOSlA9j8L._SL1500_.jpg",
@@ -113,8 +186,8 @@ const storefrontDatabase = [
 },
    {
     id: "ugp2",
-    category: "graduation",
-    subjects: "Physics",
+    category: ["all","graduation"],
+    subjects: ["Physics"],
     title: "Optics by Ajoy Ghatak",
     desc: "Optics, the book, is a classic text designed to provide a comprehensive exploration of light and its interaction with matter.",
     image: "https://m.media-amazon.com/images/I/61m1s07DjZL._SL1500_.jpg",
