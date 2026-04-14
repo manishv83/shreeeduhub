58cb73661bab67de9b2d1b9d15ea176d32206866
manishv83 <50191094+manishv83@users.noreply.github.com>
2026-04-14 15:46:41 +0530

    Add files via upload

--- data/store-db.js
+++ data/store-db.js
@@ -9,17 +9,48 @@ const storefrontDatabase = [
         link: "https://amzn.to/4dOcZl9",
         badge: "Essential"
     },
+   {
+        id: "p2",
+        category: ["all","primary"],
+        subjects: ["English", "Grammar"], 
+        title: "MTG ISO-IMO-IEO-ICSO-IGKO-ISSO Olympiad and Reasoning Workbook Class 3 (Set of 7)",
+        desc: "MTG presents the fully revised & updated “Olympiad Workbook” combo of 7 books for ISO, IMO, IEO, ICSO, IGKO, ISSO and Reasoning to ace the SOF Olympiad exams (2026-27) exam.",
+        image: "https://m.media-amazon.com/images/I/61A25FqVUkL.jpg",
+        link: "https://amzn.to/4dHHKIl",
+        badge: "Essential"
+    },
+     {
+        id: "p3",
+        category: ["all","primary"],
+        subjects: ["English", "Math", "Science", "Grammar"], 
+        title: "MTG ISO-IMO-IEO (Science, Mathematics & English) Olympiad Workbook Class 3 (Set of 3 Books)",
+        desc: "MTG’s fully revised & updated “Olympiad Workbook” combo of 3 books for ISO, IMO & IEO - the perfect tool to help students prepare for their upcoming SOF Olympiad (2026-27) exam.",
+        image: "https://m.media-amazon.com/images/I/81PFC04vDsL._SL1500_.jpg",
+        link: "https://amzn.to/4cILXKI",
+        badge: "Expert Choice"
+    },
 
+    {
+        id: "p4",
+        category: ["all","primary"],
+        subjects: ["Math", "Science", "GK"], 
+        title: "OOswaal One for All Olympiads | Class 6 Maths, Science, GK ",
+        desc: "Prep-Guide With Level 1, 2 & Achievers Section (Set of 3 Books).",
+        image: "https://m.media-amazon.com/images/I/61BkNnRWp2L._SL1001_.jpg",
+        link: "https://amzn.to/4vjzi8m",
+        badge: "Expert Choice"
+    },
     {
         id: "s1",
         category: ["all","secondary"],
-        subjects: ["English", "Grammar"],  
+        subjects: ["English", "Science", "Grammar"],  
         title: "Wren & Martin Grammar",
         desc: "Improve your English writing and grammar skills for all classes.",
         image: "https://m.media-amazon.com/images/I/616tJ8oyRRL._SL1500_.jpg",
         link: "https://amzn.to/4tvDY9v",
         badge: "Expert Choice"
     },
+
     {
         id: "h1",
         category: ["all","higher"],
@@ -32,6 +63,26 @@ const storefrontDatabase = [
     },
     {
         id: "h2",
+        category: ["all","higher"],
+        subjects: ["Physics"], 
+        title: "Concept of Physics by H.C Verma Part - I",
+        desc: "Physics as a subject is vast and to have all the concepts of Physics compiled in one book is indeed a boon to all students. The book has been tailored to meet the needs 10 + 2 or higher secondary students and for the students who are appearing for a competitive examination. It has everything a student would possibly need for their success in exams.",
+        image: "https://m.media-amazon.com/images/I/91ccGUt2sbL._SL1500_.jpg",
+        link: "https://amzn.to/4vuQjwn",
+        badge: "Top Rated"
+    },
+    {
+        id: "h3",
+        category: ["all","higher"],
+        subjects: ["Physics"], 
+        title: "Concept of Physics by H.C Verma Part - II",
+        desc: "It is an ideal book not only for pre-college students but also for those appearing for competitive exams in the field of engineering and medicine.",
+        image: "https://m.media-amazon.com/images/I/71KUrcg5XVL._SL1400_.jpg",
+        link: "https://amzn.to/424sXjF",
+        badge: "Top Rated"
+    },
+    {
+        id: "h4",
         category: ["all","secondary"],
         subjects: ["Math"], 
         title: "R.D. Sharma Class 10 Math",
@@ -50,6 +101,15 @@ const storefrontDatabase = [
     link: "https://amzn.to/4tzTMIx",
     badge: "Expert Choice"
 },
-   
+   {
+    id: "ugp2",
+    category: "graduation",
+    subjects: "Physics",
+    title: "Optics by Ajoy Ghatak",
+    desc: "Wonderful book on waves and oscillations.",
+    image: "https://m.media-amazon.com/images/I/61m1s07DjZL._SL1500_.jpg",
+    link: "https://amzn.to/4sIm6rm",
+    badge: "Expert Choice"
+}
      
 ];
